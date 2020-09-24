5ff44aa21d0a96fc03364c696385ca0704c5de7c
alvar <alvar@DESKTOP-K8EHG61>
2020-09-24 14:14:30 +0200

    commit con cambios (estructura ficheros y clase ususrio y usuarios )

---
+++ eval_ini_inter/src/eval_ini_inter/usuario.java
@@ -0,0 +1,40 @@
+package eval_ini_inter;
+
+public class usuario {
+	
+	//ESTAdo
+	
+	String usuario;
+	String passwd;
+	
+	
+	
+	//comportamientos
+	
+	
+	
+	
+	//contructor
+	
+	public usuario(String serializado) {
+		
+		String[] ListaPartes= serializado.split(";");
+		usuario=ListaPartes[0];
+		passwd= ListaPartes[1];
+		
+	}
+	
+	
+	
+	//resto comportamientos
+	
+	//getters
+	public String getuser() {
+		return usuario;
+	}
+	
+	public String getpasswd() {
+		return passwd;
+	}
+
+}
