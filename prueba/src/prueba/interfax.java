9f85e7a585823284715c1fb19983183957cb64ea
alvar <alvar@DESKTOP-K8EHG61>
2020-09-28 09:09:33 +0200

    prueba

---
+++ prueba/src/prueba/interfax.java
@@ -0,0 +1,43 @@
+package prueba;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class interfax {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					interfax window = new interfax();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public interfax() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
